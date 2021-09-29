ad71f353b857e3bbc310ebbf2bcabd6dceb73e1f
masaxsuzu <masaxsuzu+github@gmail.com>
2021-09-29 22:13:35 +0900

    Add initial files

---
+++ src/app/components/App.tsx
@@ -0,0 +1,6 @@
+import * as React from "react";
+import * as styles from "./App.scss";
+
+export const App = () => {
+  return <h1 className={styles.title}>Hello React</h1>;
+};
---
+++ src/setupTests.ts
@@ -0,0 +1,4 @@
+import "jest-extended";
+import "jest-dom/extend-expect";
+import "react-testing-library/cleanup-after-each";
+import "@testing-library/jest-dom/extend-expect";
