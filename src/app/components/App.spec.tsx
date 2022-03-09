7353988cd6bbe904a8de0a9fdd74bc11bcf3b575
masaxsuzu <masaxsuzu+github@gmail.com>
2022-03-09 22:05:59 +0900

    Update

--- src/app/components/App.spec.tsx
+++ src/app/components/App.spec.tsx
@@ -1,8 +1,10 @@
 import React from "react";
 import { render } from "@testing-library/react";
 import { App } from "./App";
+import { createHashHistory, createMemoryHistory } from "history";
+import { BrowserRouter, Router } from "react-router-dom";
 
-test("renders learn react link", () => {
+test("renders default page", () => {
   const { getByText } = render(<App />);
   const linkElement = getByText(/Hello React/i);
   expect(linkElement.innerHTML).toBe("Hello React");
--- src/app/components/App.tsx
+++ src/app/components/App.tsx
@@ -1,6 +1,31 @@
 import * as React from "react";
+import { BrowserRouter, Navigate, Route, Routes } from "react-router-dom";
 import * as styles from "./App.scss";
 
+const ROUTER_BASENAME =
+  process.env.NODE_ENV === "development" || process.env.NODE_ENV === "test"
+    ? "/"
+    : "/mini-react-app";
+
+console.log(`process.env.NODE_ENV=${process.env.NODE_ENV}`);
+console.log(`ROUTER_BASENAME=${ROUTER_BASENAME}`);
+
 export const App = () => {
-  return <h1 className={styles.title}>Hello React</h1>;
+  return (
+    <BrowserRouter basename={ROUTER_BASENAME}>
+      <Routes>
+        <Route
+          key="0"
+          path="/"
+          element={<h1 className={styles.title}>Hello React</h1>}
+        />
+        <Route key="1" path="/*" element={<Navigate to="/404"></Navigate>} />
+        <Route
+          key="404"
+          path="/404"
+          element={<h1 className={styles.e}>404 Not Found</h1>}
+        />
+      </Routes>
+    </BrowserRouter>
+  );
 };
